4d8772d9340edb953aba3125913ac93b3fc7e986
Suvroneel Nathak <suvroneelnathak213@gmail.com>
2024-05-02 01:20:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ conn = st.connection("gsheets", type=GSheetsConnection )
 # fetching exist data ue cols = no of cols , ttl = time to live
 existing_data = conn.read(worksheet="User Data", usecols=list(range(2)), ttl=5)
 
-existing_data = existing_data.dropna(how="all")  # droping empty vals
+existing_data = existing_data.dropna(how="all")  # Droping empty vals
 
-#st.dataframe(existing_data)
-
-
+# Optionally display existing data with a checkbox
+if st.checkbox("Show Existing Data"):
+  st.dataframe(existing_data)
 
 
 nltk.download('stopwords')
@@ -37,30 +37,30 @@ ps = PorterStemmer()
 
 
 def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
-    y = []
-    # Removing Special Characters ; text is a array now
-    for i in text:
-        if i.isalnum():
-            y.append(i)
+  text = text.lower()
+  text = nltk.word_tokenize(text)
+  y = []
+  # Removing Special Characters ; text is a array now
+  for i in text:
+    if i.isalnum():
+      y.append(i)
 
-    # copying values of y to text and emptying y
-    text = y[:]
-    y.clear()
+  # copying values of y to text and emptying y
+  text = y[:]
+  y.clear()
 
-    # Removing stop words & punctutations
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
+  # Removing stop words & punctutations
+  for i in text:
+    if i not in stopwords.words('english') and i not in string.punctuation:
+      y.append(i)
 
-    text = y[:]
-    y.clear()
+  text = y[:]
+  y.clear()
 
-    for i in text:
-        y.append(ps.stem(i))
+  for i in text:
+    y.append(ps.stem(i))
 
-    return " ".join(y)
+  return " ".join(y)
 
 
 # rb - read
@@ -76,42 +76,42 @@ prediction = ""
 # If button is pressed
 if st.button('Predict'):
 
-    if not input_sms:
-        st.warning("Write a message first .")
-        st.stop()
+  if not input_sms:
+    st.warning("Write a message first .")
+    st.stop()
 
-    # 1 pre-process
+  # 1 pre-process
+  transformed_sms = transform_text(input_sms)
 
-    transformed_sms = transform_text(input_sms)
+  # 2 vectorizevector input
+  vector_input = tfidf.transform([transformed_sms])
 
-    # 2 vectorizevector input
-    vector_input = tfidf.transform([transformed_sms])
+  # 3 predict - extracting 0th item since spam are in 1 , 0
+  result = model.predict(vector_input)[0]
 
-    # 3 predict - extracting 0th item since spam are in 1 , 0
-    result = model.predict(vector_input)[0]
+  # 4 Display
+  if result == 1:
+    st.header("Spam")
+    prediction = "Spam"
 
-    # 4 Display
-    if result == 1:
-        st.header("Spam")
-        prediction = "Spam"
+  else:
+    st.header("Not spam")
+    prediction = "Not spam"
 
-    else:
-        st.header("Not spam")
-        prediction = "Not spam"
+  input_data = pd.DataFrame(
+    [
+      {
+        "Input Sentence": input_sms,
+        "Output": prediction
+      }
+    ]
+  )
 
-    input_data = pd.DataFrame(
-        [
-            {
-                "Input Sentence": input_sms,
-                "Output": prediction
-            }
-        ]
-    )
-    #add inputdata to existing data
-    updated_df= pd.concat([existing_data,input_data],ignore_index=True)
+  # Add input data to existing data
+  updated_df = pd.concat([existing_data, input_data], ignore_index=True)
 
-    #update goggle sheets
-    conn.update(worksheet="User Data",data = updated_df)
+  # Update google sheets
+  conn.update(worksheet="User Data", data=updated_df)
 
 
 
@@ -119,19 +119,20 @@ theme_bg_color = st.get_option("theme.backgroundColor")
 
 # Footer
 st.markdown(f"""
-    <style>
-        .footer {{
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-            background-color: {theme_bg_color};
-            text-align: center;
-            padding: 10px;
-        }}
-    </style>
-    <div class="footer">
-        <p style="font-size: 16px;">Created by Suvroneel Nathak</p>
-    </div>
+  <style>
+    .footer {{
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      background-color: {theme_bg_color};
+      text-align: center;
+      padding: 10px;
+    }}
+  </style>
+  <div class="footer">
+    <p style="font-size: 16px;">Created by Suvroneel Nathak</p>
+  </div>
 """, unsafe_allow_html=True)
+
 
