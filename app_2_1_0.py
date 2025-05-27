9e721da1f15636ed8041a958b941eb03467f2775
Suvroneel Nathak <suvroneelnathak213@gmail.com>
2025-05-27 13:07:12 +0530

    Update app_2_1_0.py

--- app_2_1_0.py
+++ app_2_1_0.py
@@ -1,139 +1,233 @@
 import base64
+
 import nltk
 from textblob import TextBlob
 import toml
+# GSheets lib
 from streamlit_gsheets import GSheetsConnection
+
 import streamlit as st
 import pickle
 import string
 import pandas as pd
+from pandas import *
 
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+from nltk import *
+from nltk.corpus import *
+from string import *
 
-# Google Sheets connection
+
+
+
+# Establishing Google sheets connection
+
 conn = st.connection("gsheets", type=GSheetsConnection)
+
+# fetching exist data ue cols = no of cols , ttl = time to live
 existing_data = conn.read(worksheet="User Data", usecols=list(range(2)), ttl=5)
-existing_data = existing_data.dropna(how="all")
+
+existing_data = existing_data.dropna(how="all")  # droping empty vals
+
+# st.dataframe(existing_data)
+
 
 nltk.download('stopwords')
 nltk.download('punkt')
-ps = PorterStemmer()
 
-# Background setup
+ps = PorterStemmer()
+#####backgrouund###
+
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
+
 def set_background(png_file):
     bin_str = get_base64(png_file)
-    page_bg_img = f'''
+    page_bg_img = '''
     <style>
-    .stApp {{
-        background-image: url("data:image/png;base64,{bin_str}");
-        background-size: cover;
-    }}
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
     </style>
-    '''
+    ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 set_background('13.jpg')
 
-# Custom styles
-st.markdown("""
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <style>
-        html, body, [class*="css"]  {
-            font-family: 'Poppins', sans-serif;
-        }
-        .main-container {
-            background-color: rgba(255, 255, 255, 0.85);
-            padding: 2rem;
-            border-radius: 15px;
-            box-shadow: 0px 4px 12px rgba(0,0,0,0.2);
-            margin-top: 120px;
-        }
-        .navbar-custom {
-            background-color: rgba(0, 0, 0, 0.8);
-            padding: 1rem;
-            position: fixed;
-            top: 0;
-            width: 100%;
-            z-index: 1000;
-        }
-        .navbar-custom a {
-            color: white;
-            margin-right: 15px;
-            text-decoration: none;
-            font-weight: bold;
-        }
-    </style>
-    <div class="navbar-custom">
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-        <a href="https://github.com/Suvroneel">Other Projects</a>
+
+
+################
+###############Navbar####################
+
+
+# st.set_page_config(layout="wide")
+
+
+st.markdown(
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">',
+    unsafe_allow_html=True)
+
+nav_bar = """
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark"
+         style="background-color: black">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="https://www.linkedin.com/in/suvroneel-nathak-593602197/" >Suvroneel Nathak</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active " aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/Suvroneel">My Other Projects</a>
+        </li> 
+      </ul>
     </div>
-""", unsafe_allow_html=True)
+  </div>
+</nav>
+"""
+st.markdown(nav_bar, unsafe_allow_html=True)
 
-# Main container
-st.markdown('<div class="main-container">', unsafe_allow_html=True)
+# Short description of the app
 
-st.title(" Spam Email Classifier (v 2.1.0) - Test")
+# set streamlit header z-index
+st.markdown('''
+<style>
+.stApp header {
+    z-index: 0;
+}
+</style>
+''', unsafe_allow_html=True)
 
-input_sms = st.text_area("Enter the message")
 
-# Load model
+################################################
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    # Removing Special Characters ; text is a array now
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    # copying values of y to text and emptying y
+    text = y[:]
+    y.clear()
+
+    # Removing stop words & punctutations
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+# rb - read
+
 tfidf = pickle.load(open('./Model/vectorizer_mkii.pkl', 'rb'))
 model = pickle.load(open('./Model/model_mkii.pkl', 'rb'))
 
-# Text preprocessing
-def transform_text(text):
-    text = text.lower()
-    text = word_tokenize(text)
-    y = [i for i in text if i.isalnum()]
-    y = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
-    y = [ps.stem(i) for i in y]
-    return " ".join(y)
+st.title("Spam Email Classifier (v 2.1.0)")
+
+
+
+input_sms = st.text_area("Enter the message")  # takes input message
 
 prediction = ""
+
 b = TextBlob(input_sms)
 
-if st.button('🚀 Predict'):
+# If button is pressed
+if st.button('Predict'):
+
     if not input_sms:
-        st.warning("Write a message first.")
+        st.warning("Write a message first .")
         st.stop()
 
     if b.correct() == input_sms:
+
+        # 1 pre-process
+
         transformed_sms = transform_text(input_sms)
+
+        # 2 vectorizevector input
         vector_input = tfidf.transform([transformed_sms])
+
+        # 3 predict - extracting 0th item since spam are in 1 , 0
         result = model.predict(vector_input)[0]
 
+        # 4 Display
         if result == 1:
-            st.error("🚨 This looks like **Spam**!")
+            st.header("Spam")
             prediction = "Spam"
+
         else:
-            st.success("✅ This looks **Safe** (Not Spam)")
+            st.header("Not spam")
             prediction = "Not spam"
+
+        input_data = pd.DataFrame(
+            [
+                {
+                    "Input Sentence": input_sms,
+                    "Output": prediction
+                }
+            ]
+        )
+        # add inputdata to existing data
+        updated_df = pd.concat([existing_data, input_data], ignore_index=True)
+
+        # update goggle sheets
+        conn.update(worksheet="User Data", data=updated_df)
+
     else:
-        st.error("🚨 This looks like **Spam**!")
+        st.header("Spam")
         prediction = "Spam"
+        input_data = pd.DataFrame(
+            [
+                {
+                    "Input Sentence": input_sms,
+                    "Output": prediction
+                }
+            ]
+        )
+        # add inputdata to existing data
+        updated_df = pd.concat([existing_data, input_data], ignore_index=True)
+        conn.update(worksheet="User Data", data=updated_df)
 
-    input_data = pd.DataFrame([
-        {"Input Sentence": input_sms, "Output": prediction}
-    ])
-    updated_df = pd.concat([existing_data, input_data], ignore_index=True)
-    conn.update(worksheet="User Data", data=updated_df)
-
-st.markdown('</div>', unsafe_allow_html=True)
+theme_bg_color = st.get_option("theme.backgroundColor")
 
 # Footer
-st.markdown("""
-    <hr>
-    <div style='text-align: center; padding: 10px; font-size: 14px;'>
-        🚀 Made by <a href="https://www.linkedin.com/in/suvroneel-nathak-593602197/" target="_blank">Suvroneel Nathak</a> |
-        <a href="https://github.com/Suvroneel" target="_blank">GitHub</a>
+st.markdown(f"""
+    <style>
+        .footer {{
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: {theme_bg_color};
+            text-align: center;
+            padding: 10px;
+        }}
+    </style>
+    <div class="footer">
+        <p style="font-size: 16px;">Created by Suvroneel Nathak</p>
     </div>
 """, unsafe_allow_html=True)
